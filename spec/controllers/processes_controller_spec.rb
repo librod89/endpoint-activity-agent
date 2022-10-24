f5b49e2f96bc2ca4844669d69b1fc323e963ba11
Libby Rodriguez <librod89@gmail.com>
2022-10-24 13:07:15 -0600

    tweak tests

--- spec/controllers/processes_controller_spec.rb
+++ spec/controllers/processes_controller_spec.rb
@@ -3,15 +3,15 @@ require 'rails_helper'
 describe ProcessesController do
   context '#start' do
     it 'does not call exec if the path param is empty' do
-      post :start
-
       expect(subject).not_to receive(:fork)
+
+      post :start
     end
 
     context 'when path params are included' do
       let(:path) { Rails.root.join('log', 'development.log').to_s }
 
-      it 'calls exec with no args' do
+      it 'calls exec with expected path but no args' do
         expect(subject).to receive(:fork).and_yield do |block|
           expect(block).to receive(:exec).with('open', path, '')
         end
